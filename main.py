cc649f72c38aa0e2a06e25c866db7fb45d638413
Jurrian <jurrdb@gmail.com>
2021-11-19 10:37:23 +0100

    Implemented compute_total_profile

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
+import numpy as np
+
+
+def compute_total_profile(sequences, sequence_length):
+    number_of_sequences = len(sequences)
+    print('Number of sequences:', number_of_sequences)
+
+    # Total profile is an array of dictionaries. Each index in the array represents a position in the sequence, and
+    # each dictionary represents the frequency of nucleotides in that position.
+    total_profile = []
+    for i in range(0, sequence_length):
+        total_profile.append({
+            'A': 0.0,
+            'C': 0.0,
+            'T': 0.0,
+            'G': 0.0,
+        })
+
+    for key, sequence in sequences.items():
+        # Iterate over every sequence
+        for i, nt in enumerate(sequence):
+            total_profile[i][nt] += 1.0 / number_of_sequences
+
+    return total_profile
+
 
 def parse_input():
     with open('data/test-small.aln') as f:
@@ -5,13 +30,17 @@ def parse_input():
         sequences = {}
         for i in range(0, len(lines), 2):
             sequences[lines[i].strip()] =  lines[i + 1].strip()
-        return sequences
+        sequence_length = len(lines[1].strip())
+        return sequences, sequence_length
 
 
-def run(sequences):
-    pass
+def run(sequences, sequence_length):
+    total_profile = compute_total_profile(sequences, sequence_length)
+    print('Total profile:')
+    for nt_profile in total_profile:
+        print(nt_profile)
 
 
 if __name__ == '__main__':
-    sequences = parse_input()
-    run(sequences)
+    sequences, sequence_length = parse_input()
+    run(sequences, sequence_length)
